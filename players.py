2f4470fd9c735719b33d16e733db88e887fd2d6c
engineer_tacky <tsubasa801@gmail.com>
2022-02-19 17:20:29 +0900

    スライスを使ったループ処理

--- players.py
+++ players.py
@@ -1,6 +1,4 @@
 players = ['charles', 'martina', 'michael', 'florence', 'eli']
-print(players[0:3])
-print(players[1:4])
-print(players[:4])
-print(players[2:])
-print(players[-3:])
+print("チームの最初の三人の選手です。")
+for player in players[:3]:
+    print(player.title())
