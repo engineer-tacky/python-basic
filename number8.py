6fc6f08cbad8ef59521c12ccb2627f6472928dbe
engineer_tacky <tsubasa801@gmail.com>
2022-02-19 15:42:28 +0900

    コメントを入れる練習

--- number8.py
+++ number8.py
@@ -1,11 +1,10 @@
-from email import message
-
-
+# 8になるように、計算式を立てる
 print(5+3)
 print(13-5)
 print(32/4)
 print(2*4)
 
+# f-stringを使用して、メッセージを構築
 num = 5
 message = f"私の好きな数字は{num}です。"
 print(message)
