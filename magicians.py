5d6d985d9ed2588ce0cc478e66fe1419a80e9f5a
engineer_tacky <tsubasa801@gmail.com>
2022-02-19 16:46:10 +0900

    リストをループで表示する

---
+++ magicians.py
@@ -0,0 +1,7 @@
+magicians = ['alice', 'david', 'carolina']
+
+# インデントが重要！
+for magician in magicians:
+    print(f"{magician.title()}は素晴らしい手品を演じた。")
+    print(f"私は{magician.title()}の次の手品が待ちきれない。\n")
+print("みなさんありがとう。素晴らしい手品ショーでした！")
