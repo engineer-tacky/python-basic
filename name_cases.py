40b1c88a69b580bdffbbcbff7bb033eb1c33019e
engineer_tacky <tsubasa801@gmail.com>
2022-02-19 15:35:58 +0900

    文字列を扱う練習問題

---
+++ name_cases.py
@@ -0,0 +1,17 @@
+name = "Eric"
+message = f"こんにちは、{name}。今日はPythonを学びますか？"
+print(message)
+
+name = "steven johnson"
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+name = "アインシュタイン"
+message = f'{name}は、"相対性理論"を発明した'
+print(message)
+
+name = "  steven Johnson  "
+print(name.rstrip())
+print(name.lstrip())
+print(name.strip())
